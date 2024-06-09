d81fc5fbfe9442d0f7d804718b659c12842d5e0f
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-09 17:42:21 +0700

    Add: setup tauri + react + shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-500 text-xl">Hello World</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
