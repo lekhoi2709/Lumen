1d579c18dd64445128909b9439365318d7385c2a
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-10 11:15:39 +0700

    Add: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import "./App.css";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
 import AuthLayout from "@/components/auth-layout";
-import SplashScreen from "./components/splash-screen";
+import SplashScreen from "@/components/splash-screen";
 
 function App() {
   const [showSplash, setShowSplash] = useState(
-    () => !JSON.parse(localStorage.getItem("isFirstTimeShowSplash") || "false")
+    () =>
+      !JSON.parse(sessionStorage.getItem("isFirstTimeShowSplash") || "false")
   );
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      localStorage.setItem("isFirstTimeShowSplash", JSON.stringify(true));
+      sessionStorage.setItem("isFirstTimeShowSplash", JSON.stringify(true));
       setShowSplash(false);
     }, 2000);
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+  {
+    path: "/contact",
+    element: <h1>Contact</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider defaultTheme="light">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
 );
