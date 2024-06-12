b9e266777e658a434141e5196e0976a3fc61f5d6
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-12 11:46:16 +0700

    Add: create auth route

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/login", (req, res) => {});
+router.get("/register", (req, res) => {});
+router.get("/logout", (req, res) => {});
