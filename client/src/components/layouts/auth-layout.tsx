f32b6ae97eb9b15a510d5cfd0963f0a02630fa7f
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-14 10:21:43 +0700

    fix: unexpected UI on mobile

--- client/src/components/layouts/auth-layout.tsx
+++ client/src/components/layouts/auth-layout.tsx
@@ -3,7 +3,7 @@ import AuthBg from "@/assets/auth-bg.jpg";
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen min-h-screen flex items-center justify-center relative">
       <div className="w-full h-full absolute z-0">
         <img
           src={AuthBg}
@@ -12,9 +12,7 @@ function AuthLayout({ children }: { children: React.ReactNode }) {
           loading="eager"
         />
       </div>
-      <section className="z-10 w-full h-full flex items-center justify-center">
-        {children}
-      </section>
+      {children}
       <span className="absolute z-10 bottom-0 right-0 p-2 lg:p-3 bg-background/90 backdrop-blur-sm rounded-tl-md text-sm lg:text-base">
         <p>
           Designed by{" "}
