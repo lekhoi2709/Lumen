dbcca01e85b0ce09553874c262edcb02179644f9
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-15 22:22:49 +0700

    feat: handle CORS

--- server/src/express.config.ts
+++ server/src/express.config.ts
@@ -1,7 +1,20 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const ExpressConfig = (): Application => {
   const app = express();
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
