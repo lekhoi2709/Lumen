cfddc43d3929b580ab730a8f2a88e869b7d6d6b4
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-07-02 12:14:22 +0700

    feat: add cookie-parser

--- server/src/express.config.ts
+++ server/src/express.config.ts
@@ -1,11 +1,15 @@
 import express, { Application } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 const ExpressConfig = (): Application => {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use("/", express.static(path.join(__dirname, "public")));
+  app.use(cookieParser());
 
   app.use(
     cors({
