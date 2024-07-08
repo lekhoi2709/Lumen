4942f248070d8f46463258fbd7f2e10143d4bb99
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-07-08 19:44:27 +0700

    feat: adjust footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@/contexts/theme-provider";
 import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 
@@ -60,17 +61,23 @@ function Footer({ className }: { className?: string }) {
       ],
     },
   ];
+  const themeSetting = useTheme();
+
   return (
     <footer
       className={twMerge(
-        "rounded-t-md w-full flex flex-col items-center justify-center p-6 bg-orange-500 dark:bg-orange-600 text-[#121212] gap-8",
+        "rounded-t-md w-full flex flex-col items-center justify-center p-6 bg-orange-500 gap-8",
         className
       )}
     >
-      <div className="w-full max-w-[900px] flex flex-wrap items-center justify-center text-sm gap-8 md:flex-row md:items-start md:text-base md:gap-12">
+      <div className="w-full max-w-[900px] flex flex-wrap items-start justify-center text-sm gap-8 md:flex-row md:text-base md:gap-12 text-foreground">
         <div className="relative w-32 md:w-48">
           <img
-            src="/logo/logo-no-background.png"
+            src={
+              themeSetting.theme === "light"
+                ? "/logo/logo-no-background.png"
+                : "/logo/logo-no-bg-white.png"
+            }
             alt="Lumen Logo"
             className="h-auto max-w-full object-fill"
             loading="eager"
@@ -79,10 +86,10 @@ function Footer({ className }: { className?: string }) {
         {data.map((section, index) => (
           <div
             key={index}
-            className="flex flex-col gap-6 items-center md:items-start"
+            className="flex flex-col gap-6 items-start max-w-32 w-32 md:w-auto md:max-w-none"
           >
             <h3 className="text-lg font-bold">{section.title}</h3>
-            <ul className="flex flex-col items-center gap-2 md:items-start">
+            <ul className="flex flex-col gap-2 items-start">
               {section.items.map((item, index) => (
                 <li key={index}>
                   <a href={item.link}>{item.title}</a>
@@ -94,7 +101,7 @@ function Footer({ className }: { className?: string }) {
       </div>
       <div className="row">
         <div className="col-md-12">
-          <p className="text-xs md:text-sm">
+          <p className="text-xs md:text-sm text-foreground">
             2024 Lumen LMS. Built by LDK, VLTP
           </p>
         </div>
