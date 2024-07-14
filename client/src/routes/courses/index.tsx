831da22feb454cdad81edd3afbe27af8739a13a4
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-07-14 23:32:58 +0700

    feat: create, join courses UI

--- client/src/routes/courses/index.tsx
+++ client/src/routes/courses/index.tsx
@@ -1,10 +1,11 @@
 import Layout from "@/layouts/layout";
+import Title from "@/components/courses/title";
 
 function CoursesPage() {
   return (
     <Layout>
-      <div className="w-full p-6">
-        <h1>Courses</h1>
+      <div className="w-full p-6 pr-4 md:p-6 md:pl-[224px]">
+        <Title />
       </div>
     </Layout>
   );
