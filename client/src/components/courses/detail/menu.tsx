be06db7238c747ea249154a00d80e9ff39a68a89
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-07-28 12:23:07 +0700

    feat: menu animation

--- client/src/components/courses/detail/menu.tsx
+++ client/src/components/courses/detail/menu.tsx
@@ -3,27 +3,67 @@ import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function CourseMenu() {
   const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
 
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scrolling down, hide the navbar
+        setShow(false);
+      } else {
+        // if scrolling up, show the navbar
+        setShow(true);
+      }
+
+      // remember the current page location for the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <ScrollArea className="w-full whitespace-nowrap">
-      <div className="flex flex-row justify-center px-4 border-b border-border w-full z-10 bg-background md:fixed md:ml-[12rem] md:justify-start md:gap-0 md:pl-8 text-base lg:ml-[14rem]">
-        <StyledNavLink path={`/courses/${id}`}>
-          {t("courses.detail.overview")}
-        </StyledNavLink>
-        <StyledNavLink path={`/courses/${id}/assignments`}>
-          {t("courses.detail.assignments")}
-        </StyledNavLink>
-        <StyledNavLink path={`/courses/${id}/people`}>
-          {t("courses.detail.people")}
-        </StyledNavLink>
-        <StyledNavLink path={`/courses/${id}/grades`}>
-          {t("courses.detail.grades")}
-        </StyledNavLink>
+    <ScrollArea className="relative w-full whitespace-nowrap">
+      <div
+        className={twMerge(
+          "fixed z-10 w-full overflow-x-auto bg-background transition-all duration-300",
+          show
+            ? "translate-y-0"
+            : "-translate-y-full bg-transparent md:translate-y-0 md:bg-background",
+        )}
+      >
+        <div className="z-10 flex flex-row justify-evenly border-b border-border bg-transparent px-4 text-base transition-colors duration-300 md:ml-[12rem] md:justify-start md:gap-0 md:bg-background md:pl-8 lg:ml-[14rem]">
+          <StyledNavLink path={`/courses/${id}`}>
+            {t("courses.detail.overview")}
+          </StyledNavLink>
+          <StyledNavLink path={`/courses/${id}/assignments`}>
+            {t("courses.detail.assignments")}
+          </StyledNavLink>
+          <StyledNavLink path={`/courses/${id}/people`}>
+            {t("courses.detail.people")}
+          </StyledNavLink>
+          <StyledNavLink path={`/courses/${id}/grades`}>
+            {t("courses.detail.grades")}
+          </StyledNavLink>
+        </div>
       </div>
+
       <ScrollBar orientation="horizontal" />
     </ScrollArea>
   );
@@ -43,11 +83,11 @@ function StyledNavLink({
       to={path}
       className={({ isActive }) =>
         twMerge(
-          "h-14 flex flex-col items-center justify-center hover:cursor-pointer",
+          "flex h-14 flex-col items-center justify-center hover:cursor-pointer",
           isActive
-            ? "text-orange-500 after:content-[''] after:w-full after:h-1 after:rounded-t-md after:bg-orange-500 after:translate-y-[0.9rem] hover:bg-orange-500/20"
+            ? "text-orange-500 after:h-1 after:w-full after:translate-y-[0.9rem] after:rounded-t-md after:bg-orange-500 after:content-[''] hover:bg-orange-500/20"
             : "hover:bg-muted",
-          className
+          className,
         )
       }
       onClick={() => sessionStorage.setItem("history", path)}
