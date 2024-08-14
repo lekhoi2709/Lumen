d02cb5fe3aebde778bf4ebfcc66942e286a1c591
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-08-14 21:01:47 +0700

    feat: add axios tauri adapter to able to use axios in tauri app

--- client/src/services/api/axios-instance.ts
+++ client/src/services/api/axios-instance.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { isTauri } from "@/lib/utils";
+import axiosTauriApiAdapter from "axios-tauri-api-adapter";
 
 const isRunningInTauri = isTauri() !== undefined;
 
 const BASE_URL = isRunningInTauri
   ? (import.meta.env.VITE_API_URL as string)
   : process.env.API_URL;
-export const axiosInstance = axios.create({ baseURL: BASE_URL });
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  adapter: isRunningInTauri ? axiosTauriApiAdapter : undefined,
+});
