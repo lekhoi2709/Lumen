d0cd657b7ea93d9b6f5765b46fa7a4431733bd03
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-08-15 14:25:52 +0700

    feat: add plugin shell to open external links

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -2,16 +2,17 @@
 extern crate dotenv;
 
 pub fn run() {
-  if cfg!(debug_assertions) {
-    dotenv::from_filename(".env.development").unwrap().load();
-  } else {
-    let prod_env: &str = include_str!("../../.env.production");
-    let result = dotenv::from_read(prod_env.as_bytes()).unwrap();
-    result.load();
-  }
+    if cfg!(debug_assertions) {
+        dotenv::from_filename(".env.development").unwrap().load();
+    } else {
+        let prod_env: &str = include_str!("../../.env.production");
+        let result = dotenv::from_read(prod_env.as_bytes()).unwrap();
+        result.load();
+    }
 
-  tauri::Builder::default()
-    .plugin(tauri_plugin_http::init())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_http::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
