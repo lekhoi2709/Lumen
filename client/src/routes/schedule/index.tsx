c896b9b0dcd2042488e568a238d492ba094d0d18
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-08-19 19:29:12 +0700

    feat: custom event calendar

--- client/src/routes/schedule/index.tsx
+++ client/src/routes/schedule/index.tsx
@@ -9,6 +9,7 @@ import { useSchedulesAndAssignments } from "@/services/queries/schedule";
 import Loading from "@/components/loading";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 function SchedulePage() {
   const { data, isLoading, error } = useSchedulesAndAssignments();
@@ -102,10 +103,67 @@ function SchedulePage() {
               hour12: false,
             }}
             eventColor="#f97316"
+            eventContent={CustomEvent}
           />
         </div>
       </div>
     </Layout>
+  );
+}
+
+function CustomEvent(info: any) {
+  const isDayGrid = info.view.type === "dayGridMonth";
+  const isTimeGrid = info.view.type === "timeGridWeek";
+
+  const addOneHourToTimeText = (timeText: string) => {
+    const [hours, minutes] = timeText.split(":").map(Number);
+    const date = new Date();
+
+    date.setHours(hours);
+    date.setMinutes(minutes);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    date.setHours(date.getHours() + 1);
+
+    const updatedHours = date.getHours().toString().padStart(2, "0");
+    const updatedMinutes = date.getMinutes().toString().padStart(2, "0");
+
+    return `${updatedHours}:${updatedMinutes}`;
+  };
+
+  return (
+    <div
+      className={twMerge(
+        "h-full w-full cursor-pointer truncate rounded-md p-1",
+        isTimeGrid &&
+          "bg-orange-500 transition-shadow duration-200 ease-in-out hover:shadow-xl",
+        isDayGrid && "",
+      )}
+    >
+      {isDayGrid && (
+        <div className="w-full">
+          <span className="relative inline-flex items-center pl-5 before:absolute before:left-0 before:top-1/2 before:h-2 before:w-2 before:-translate-y-1/2 before:rounded-full before:bg-orange-500 before:content-['']">
+            <p className="hidden md:inline-block">
+              {addOneHourToTimeText(info.timeText)}{" "}
+            </p>
+            <strong className="font-bold">
+              <p className="mx-1 hidden md:inline-block">{" - "}</p>
+              {info.event.title}
+            </strong>
+          </span>
+        </div>
+      )}
+      {isTimeGrid && (
+        <div className="flex max-h-full flex-col items-center">
+          <span className="w-full truncate">
+            {info.timeText.split(" - ")[1]}
+          </span>
+          <strong className="w-full truncate font-bold">
+            {info.event.title}
+          </strong>
+        </div>
+      )}
+    </div>
   );
 }
 
